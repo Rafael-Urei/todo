d27ec70249122222b6cf05a35a4ddcf2572dac1d
Rafael-Urei <urei.dev@outlook.com>
2023-11-16 15:01:06 -0300

    :frog: Created the add task service

---
+++ app/shared/services/tasks.ts
@@ -0,0 +1,18 @@
+import { Dispatch, SetStateAction } from "react";
+import { TasksType } from "../types/Tasks";
+import { notifyIfFailed, notifyIfSuccess } from "../utils/Toasts";
+import {v4} from 'uuid';
+
+export const createTask = (data: TasksType, setTasks: Dispatch<SetStateAction<TasksType[]>>) => {
+    try {
+        setTasks(prev => {
+            return [
+                ...prev,
+                {...data, id: v4()}
+            ]
+        })
+        notifyIfSuccess('Task created with success!')
+    } catch (error) {
+        notifyIfFailed('An error has occurred while creating your task, please try again.')
+    }
+};
